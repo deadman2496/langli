7e59eb8c8cb5e8eaed58acbf000b5cab60c48d0d
deadman2496 <a.marroquin2496@gmail.com>
2024-03-26 22:27:45 -0400

    Worked on schema fixed some lines

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -5,7 +5,7 @@ import { neon } from "@neondatabase/serverless";
 import * as schema from "../db/schema";
 
 const sql = neon(process.env.DATABASE_URL!);
-// @ts-ignore
+
 const db = drizzle(sql, { schema });
 
 const main = async () => {
